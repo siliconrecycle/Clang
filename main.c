7a33e107bfc7c6c5ba133e7f9d5da385c7902a33
Yuan <2252292157@qq.com>
2022-07-18 18:21:06 +0800

    25.2.9

--- main.c
+++ main.c
@@ -1,26 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-struct record {
-	char name[10];
-	int age;
-};
 
 int main(void)
 {
-	struct record array[] = {{"shen", 26}, {"lv", 27}};
-	FILE *fp = fopen("recfile", "w+");
-	if (fp == NULL) {
-		perror("Open file recfile");
-		exit(1);
-	}
-	fwrite(array, sizeof(struct record), 2, fp);
-
-	fread(array, sizeof(struct record), 2, fp);
-	printf("Name1: %s\tAge1: %d\n", array[0].name, array[0].age);
-	printf("Name2: %s\tAge2: %d\n", array[1].name, array[1].age);
-	
-
-	fclose(fp);
-	return 0;
+	char month[100];
+	int day, year;
+	char *date = "18 Jul 2022";
+	sscanf(date, "%d %s %d", &day, month, &year);
+	printf("%d/%s/%d\n", day, month, year);
 }
