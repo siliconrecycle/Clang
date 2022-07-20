b0b98f9f67e4728a7ba4db112778c1fd7ff17e95
Yuan <2252292157@qq.com>
2022-07-20 09:46:14 +0800

    30.2

--- main.c
+++ main.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
 
 int main(void)
 {
-	struct winsize size;
-	if (isatty(STDOUT_FILENO) == 0)
-		exit(1);
-	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &size) < 0) {
-		perror("ioctl TIOCGWINSZ error");
-		exit(1);
+	extern char **environ;
+	int i;
+	for(i = 0; environ[i] != NULL; i++) {
+		printf("%s\n", environ[i]);
 	}
-	printf("%d rows, %d columns\n", size.ws_row, size.ws_col);
+	printf("PATH=%s\n", getenv("PATH"));
 	return 0;
 }
