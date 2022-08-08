cdbaf9dd767ee6dc9de3eb2dabb3a3f80f6e8d47
Yuan <2252292157@qq.com>
2022-08-08 16:59:49 +0800

    ex 1-6, 1-7

--- 1.5.c
+++ 1.5.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 int main() {
-	int c = getchar();
+	int c;
 
-	while (c != EOF) {
+	/* putchar(33); */
+	/* printf("true or false: %d\n", c = getchar() != EOF); */
+	/* (c = getchar != EOF) => 1(true)
+	This is very important, 1 in ascii code is a no-display symbol.
+	That means, even if it is in infinit loop, it will display nothing
+	in terminal.
+	Of course, we also need to know in function `putchar()`, his argument
+	is integer, and then be transfered to character by ascii code table.*/
+	while ((c = getchar()) != EOF) { 
 		putchar(c);
-		c = getchar();
 	}
+
 }
---
+++ ex.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int inttype;
+	char chartype;
+
+	printf("int: %d byte\n", sizeof(inttype));
+	printf("char: %d byte\n", sizeof(chartype));
+	printf("EOF: %d\n", EOF);
+
+}
+	
