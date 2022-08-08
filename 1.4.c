6b42e85edbf0c0fec475896e72a867f87dcc7668
Yuan <2252292157@qq.com>
2022-08-08 11:08:31 +0800

    1.4 finished

--- 1.4.c
+++ 1.4.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 int main() {
-	printf("07082202\n");
-	
+	int fahr;
+		
+	printf("Fahr Celsius\n");
+      	for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
+		printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
+	}
 }
