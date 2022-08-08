51700a070f6f0e31646840ccdf71c628bae86949
Yuan <2252292157@qq.com>
2022-08-08 19:28:11 +0800

    1.5.3

--- 1.5.c
+++ 1.5.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
-/* In this time, we will write a program which can count the
-   number of characters.
-   This is 1.5.2, i will write this program in two ways.*/
+/* This time, we will counte the number of lines. */
+
 int main() {
-	long nc;
+	int nl;
+	int c;
 
-	/* nc = 0; */
-	/* while (getchar() != EOF) { */
-	/* 	++nc; */
-	/* } */
-
-	for (nc = 0; getchar() != EOF; ++nc)
-		;
-	printf("The number of input character: %ld\n", nc);
+	nl = 0;
+	while ((c = getchar()) != EOF) {
+		if (c == '\n')
+			++nl;
+	}
+	printf("The number of input lines: %d\n", nl);
 }
