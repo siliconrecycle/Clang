23d6b17868dd4359f48ff7079e66d870aff0fcb7
Yuan <2252292157@qq.com>
2022-08-08 20:01:06 +0800

    ex.1-9

--- 1.5.c
+++ 1.5.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
 
-/* ex.1-8 count blanks, tabs, newlines. */
+/* ex.1-9 copy input to output, and replacing one or more blanks by a single
+ blank.*/
 
 int main() {
-	int nl, nb, nt;
 	int c;
 
-	nl = nb = nt = 0;
 	while ((c = getchar()) != EOF) {
-		if (c == '\n') {
-			++nl;
-		} else if (c == ' ') {
-			++nb;
-		} else if (c == '\t') {
-			++nt;
-		}
+		putchar(c);
+		while ((c = getchar()) == ' ')
+			;
 	}
-	printf("The number of newlines: %d\n", nl);
-	printf("The number of tabs: %d\n", nt);
-	printf("The number of blanks: %d\n", nb);
 }
