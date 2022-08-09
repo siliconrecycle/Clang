1726b92e08702a1f0489204dd8289d88d312efc1
Yuan <2252292157@qq.com>
2022-08-09 19:17:48 +0800

    1.5.4

--- 1.5.c
+++ 1.5.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 
-/* ex.1-10 replace the tab with '\t', backspace with '\b', backslash with
- '\\'.*/
+/* 1.5.4 This example will count the number of lines, characters, words */
+
+#define IN  1
+#define OUT 0
 
 int main() {
-	int c;
-
+	int c, state;
+	int nc, nl, nw;
+	
+	nc = nl = nw = 0;
+	state = OUT;
 	while ((c = getchar()) != EOF) {
-		if (c == '\t') {
-			putchar('\\');
-			putchar('t');
-		} else if (c == '\b') {
-			putchar('\\');
-			putchar('b');
-		} else if (c == '\\') {
-			putchar('\\');
-			putchar('\\');
-		} else {
-			putchar(c);
+		++nc;
+		if (c == '\n')
+			++nl;
+		if (c == ' ' || c == '\t' || c == '\n') {
+			state = OUT;
+		} else if (state == OUT) {
+			state = IN;
+			++nw;
 		}
 	}
+	printf("chars: %d, lines: %d, words: %d\n", nc, nl, nw);
 }
