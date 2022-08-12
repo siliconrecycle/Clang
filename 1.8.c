be28ea6f9f0ed140ccc6876f91c45b5268cdc08b
Yuan <2252292157@qq.com>
2022-08-12 09:47:06 -0400

    fix 1.9

--- 1.8.c
+++ 1.8.c
@@ -13,8 +13,8 @@
    how can i do this (stored line = this line),and how
    can i realize (this line is longer than stored line)?
 */
+
 #define MAX 100
-#define LIM 10
 
 int getline01(char s[], int lim);
 void copy01(char dest[], char src[]);
@@ -25,7 +25,7 @@ int main() {
 	char stored[MAX];
 	
 	slen = 0;
-	while ((len = getline01(line, LIM))) {
+	while ((len = getline01(line, MAX)) > 0) {
 		if (len > slen) {
 			copy01(stored, line);
 			slen = len;
@@ -49,7 +49,7 @@ int getline01(char s[], int lim) {
 
 void copy01(char dest[], char src[]) {
 	int i;
-	for (i = 0; i < LIM && src[i] != '\0'; ++i) {
+	for (i = 0; src[i] != '\0'; ++i) {
 		dest[i] = src[i];
 	}
 	dest[i] = '\0';
