34695f3a65844df4c27d8881d20062b2e54ab9fc
Yuan <2252292157@qq.com>
2022-08-13 11:02:56 +0800

    ex.1-18

--- 1.9.c
+++ 1.9.c
@@ -1,37 +1,24 @@
-
 #include <stdio.h>
 
-/* This is a program that reads a set of text lines and prints the longest.
-   This is the outline of the program:
-
-   while(has text line) {
-           if (this line is longer than stored line) {
-	           (stored line = this line)
-           }
-   }
-   printf(stored line)
-
-   how can i do this (stored line = this line),and how
-   can i realize (this line is longer than stored line)?
-
-   ex.1-17 write a program to print all lines that are longer than 80 characters.
-
-   while (has text line) {
-           if (length of line > 80) {
-	           (copy it)
-		   (save it)
+/*
+  ex.1-17 Write a program to remove trailing blanks and tabs from
+  the each line of input and delete the entirely blank lines.
+  Outline:
+           while (has next line) {
+	           if (the line trailing is blanks or tabs) {
+		           (replace it with newline)
+			   (replaec its next char with null character)
+			   (save it)
+		   } else if (the line is the entirely blank line) {
+		           (skip it)[this means don't save it]
+		   }
 	   }
-   }
-   (print it)
+	   (print saved lines)
 
-   There is still some question, how can i save the unknown length of the lines?
-   I don't know the size of array that i need to declare, because i don't know
-   how many lines which its length longer than LIM.
-   but i can simplely print it.
+   There still have some question is where can i stored lines.
 */
 
 #define MAX 100
-#define LIM 8
 
 int getline01(char s[], int lim);
 void copy01(char dest[], char src[]);
@@ -39,22 +26,21 @@ void copy01(char dest[], char src[]);
 int main() {
 	int len, slen, longest;
 	char line[MAX];
-	char stored[MAX];
+	char stored[MAX][MAX];
 	
 	slen = longest = 0;
 	while ((len = getline01(line, MAX)) > 0) {
-		if (len == MAX - 1 && line[MAX - 1] == '\0') {
-			slen = slen + len; 
-		} else if (slen > 0) {
-			slen = len + slen;
-			if (slen > LIM) {
-				printf("%s", line);
-			}
-			slen = 0;
-		} else if (len > LIM) {
+		/* printf("%d\n", len); */
+		/* printf("s: %c\n", line[len - 2]); */
+		if (line[len - 2] == ' ' || line[len - 2] == '\t') {
+			line[len - 2] = '\n';
+			line[len - 1] = '\0';
 			printf("%s", line);
-			longest = len;
-		} 
+		} else if (len == 1 && line[len - 1] == '\n') {
+			printf("*");
+		} else {
+			printf("%s", line);
+		}
 	}
 }
 
