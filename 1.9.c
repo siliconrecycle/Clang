b45b71be016c0e143b7f50df165a03b3b93ec1e4
Yuan <2252292157@qq.com>
2022-08-13 11:27:49 +0800

    ex.1-19

--- 1.9.c
+++ 1.9.c
@@ -1,47 +1,40 @@
 #include <stdio.h>
 
 /*
-  ex.1-17 Write a program to remove trailing blanks and tabs from
-  the each line of input and delete the entirely blank lines.
+  ex.1-19 Write a function to reverse the characters of a string, use it
+  to write a program that reverse it input a line at a time.
   Outline:
-           while (has next line) {
-	           if (the line trailing is blanks or tabs) {
-		           (replace it with newline)
-			   (replaec its next char with null character)
-			   (save it)
-		   } else if (the line is the entirely blank line) {
-		           (skip it)[this means don't save it]
-		   }
-	   }
-	   (print saved lines)
-
-   There still have some question is where can i stored lines.
+          while(has a line) {
+		          (reverse)
+			  (print it)
+	  }
+	  char[] reverse(line, len)
+	          
 */
 
 #define MAX 100
 
 int getline01(char s[], int lim);
 void copy01(char dest[], char src[]);
+void reverse(char line[], int len);
 
 int main() {
 	int len, slen, longest;
 	char line[MAX];
-	char stored[MAX][MAX];
+	char stored[MAX];
 	
 	slen = longest = 0;
 	while ((len = getline01(line, MAX)) > 0) {
-		/* printf("%d\n", len); */
-		/* printf("s: %c\n", line[len - 2]); */
-		if (line[len - 2] == ' ' || line[len - 2] == '\t') {
-			line[len - 2] = '\n';
-			line[len - 1] = '\0';
-			printf("%s", line);
-		} else if (len == 1 && line[len - 1] == '\n') {
-			printf("*");
-		} else {
-			printf("%s", line);
-		}
+		reverse(line, len);
 	}
+}
+
+void reverse(char line[], int len) {
+	char r[MAX];
+	for (int i = 0; i < len; ++i) {
+		r[i] = line[len - i - 2];
+	}
+	printf("%s", r);
 }
 
 int getline01(char s[], int lim) {
