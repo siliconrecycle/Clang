600f471e99fe8de6c138ccbba084d2860786afa5
Yuan <2252292157@qq.com>
2022-08-13 23:12:51 -0400

    ex.1-21

--- 1.10.c
+++ 1.10.c
@@ -1,38 +1,48 @@
 #include <stdio.h>
 
 /*
-  ex.1-20 write a program deltab replaces the tab with the proper
-  number of space.
+  ex.1-21 write a program entab replaces the spacing with the minimal
+  number of space and tab.
   Outline:
-          while (has tab) {
-	          (print TAB - len space)
+          while (has character) {
+	          if(is space) {
+		          (count space)
+			  (replace the space with n tab and m space)
+		  }
 		  (print char)
 	  }
 
-  Note: think about the newline to sperate the line and make the counter count from
-  zero.
+  Note: This program should think about the number of space bigger than TAB and
+  smaller than TAB, this will have different result.
 */
 #define TAB 8
 
 int main() {
-	int i, c;
+	int i, c, s;
+	int n, m;
 
 	i = 0;
 	while ((c = getchar()) != EOF) {
 		++i;
-
-		if (c == '\t') {
+		if (c == ' ') {
 			--i;
-			i = i % TAB;
-			for (int j = 0; j < TAB - i; ++j) {
+			for (s = 1; (c = getchar()) == ' '; ++s);
+
+			n = (i + s) / TAB;
+			if (n > 0)
+				m = (i + s) % TAB;
+			else
+				m = (i + s) % TAB - i;
+			
+			for (int j = 0; j < n; ++j)
+				putchar('\t');
+			for (int k = 0; k < m; ++k)
 				putchar('*');
-			}
-			i = 0;
+			i = 1;
 		} else if (c == '\n') {
 			putchar(c);
 			i = 0;
-		} else {
-			putchar(c);
 		}
+		putchar(c);
 	}
 }
