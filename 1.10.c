f3c2d41561d81d4227db7a19b9d61d872a20b9e9
Yuan <2252292157@qq.com>
2022-08-13 21:21:05 -0400

    ex.1-20

--- 1.10.c
+++ 1.10.c
@@ -4,55 +4,35 @@
   ex.1-20 write a program deltab replaces the tab with the proper
   number of space.
   Outline:
-          while (has 
+          while (has tab) {
+	          (print TAB - len space)
+		  (print char)
+	  }
+
+  Note: think about the newline to sperate the line and make the counter count from
+  zero.
 */
-
-#define MAX 100
-
-char line[MAX];
-char stored[MAX];
-
-int getline01(void);
-void copy01(void);
+#define TAB 8
 
 int main() {
-	int len, slen;
-	/* extern char stored[]; */
-	
-	slen = 0;
-	while ((len = getline01()) > 0) {
-		if(len > slen) {
-			slen = len;
-			copy01();
+	int i, c;
+
+	i = 0;
+	while ((c = getchar()) != EOF) {
+		++i;
+
+		if (c == '\t') {
+			--i;
+			i = i % TAB;
+			for (int j = 0; j < TAB - i; ++j) {
+				putchar('*');
+			}
+			i = 0;
+		} else if (c == '\n') {
+			putchar(c);
+			i = 0;
+		} else {
+			putchar(c);
 		}
 	}
-
-	if (slen > 0)
-		printf("%s", stored);
-}
-
-int getline01(void) {
-	int c, i;
-	/* extern char line[MAX]; */
-
-	for (i = 0; i < MAX - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
-		line[i] = c;
-	}
-	if (c == '\n') {
-		line[i] = '\n';
-		++i;
-	}
-	line[i] = '\0';	
-	return i;
-}
-
-void copy01(void) {
-	int i;
-	/* extern char line[MAX]; */
-	/* extern char stored[MAX]; */
-
-	for (i = 0; line[i] != '\0'; ++i) {
-		stored[i] = line[i];
-	}
-	stored[i] = '\0';
 }
