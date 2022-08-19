1ac5d0e125b8d809ad6d865d409025140fa48f45
Yuan <2252292157@qq.com>
2022-08-19 17:27:32 +0800

    ex.1-24(save)

--- 1.10.c
+++ 1.10.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 
 /*
-  ex.1-23 write a program to remove comment from the C program.
+  ex.1-23 write a program to check a C program for rudimentary
+  syntax errors of the unmatched parentheses, brackets and braces.
+  
   Outline:
           while (has a character) {
-	          if (the first two characters are '/' and '*') {
-		          (skip middle character with loop
-			   until the last two character are '*'
-			   and '/')
-			   while (the first character is not '*', the
-			   last character is not '/');
-			   (print char)
-		   }
+	          (matched or not)
 	  }
-  note: this program in some aspect is simily to remove some blank into one blank.
 */
 
 int main(void) {
