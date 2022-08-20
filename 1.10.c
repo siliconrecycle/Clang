759890f84d3e6f9fa61335ccb2ea6103f31a7b8c
Yuan <2252292157@qq.com>
2022-08-20 09:56:05 -0400

    ex.1-24(finish)

--- 1.10.c
+++ 1.10.c
@@ -5,32 +5,63 @@
   syntax errors of the unmatched parentheses, brackets and braces.
   
   Outline:
+        1:
           while ((c = getchar()) != EOF) {
 	          if (c == '{') {
 		          while ((c = getchar() != '}'));
 		  }
 	  }
-
-	  match ('{', '}') {
-	          if (c == EOF) {
-		         (return error)
-		  } else if (c == '{') {
-		         c = getchar()
-			 (matched '{' and '}')
-		  } else if (c == '}') {
-		         (return success)
-		  } else {
-		         c = getchar()
-		  }
+	2:
+	  while ((c == getchar()) != EOF) {
+	          if (c == '{')
+		          (count +1)
+		  else if (c == '}')
+		          (count -1)
 	  }
+	  if (count == 0)
+	          (everything is right)
+	  else
+	          (syntax error)
+	  
+  Note: this problem should have three ways to slove.first, you can use 'digui'.
+  next, you can use 'loop'.last, you can use, i forget it.
+  This time, i write a simple program, this program only can slove unnest braces.
+  Should notice one thing. if you only input '}' rather than '{', this will not
+  say you are error.
 */
 
 int main(void) {
-	int c;
+	/* int c; */
 
-	while ((c1 = getchar()) != EOF) {
+	/* while ((c = getchar()) != EOF) { */
+	/* 	if (c == '{') { */
+	/* 		while ((c = getchar()) != '}') { */
+	/* 			if (c == EOF) { */
+	/* 				printf("Error!\n"); */
+	/* 				return 0; */
+	/* 			} */
+	/* 		} */
+	/* 		printf("Success!\n");		 */
+	/* 	} */
+	/* } */
+
+	int n, m, c;
+
+	n = m = 0;
+	while ((c = getchar()) != EOF) {
 		if (c == '{') {
-			
+			++n;
+		} else if (c == '}') {
+			--n;
+		} else if (c == '(') {
+			++m;
+		} else if (c == ')') {
+			--m;
 		}
 	}
+
+	if (n != 0 || m != 0)
+		printf("Syntax Error!\n");
+	else
+		printf("Success!\n");
 }
