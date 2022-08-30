f14f901d1877d48953d6f6557aa859c1742651bf
Yuan <2252292157@qq.com>
2022-08-30 17:18:57 +0800

    ex.2-5(finished)

--- 2.8.c
+++ 2.8.c
@@ -1,42 +1,29 @@
-/* ex.2-4 write an alternative version of squeeze(s1, s2) that delete each character in s1 that matches any character in s2.*/
+/* ex.2-5 write a program any(s1, s2), which returns the first location in a string s1 where any character from the string s2 occurs, or -1 if s1 contains no character in s2.*/
 
 #include <stdio.h>
 
-void squeeze(char s1[], char s2[]);
-/* void strcat01(char s[], char t[]); */
+int any(char s1[], char s2[]);
 
 int main(void) {
 	char s1[] = "hello world!";
-	char s2[] = "ow";
+	char s2[] = "ewo";
 	printf("%s\n", s1);
+	printf("%s\n", s2);
 
-	squeeze(s1, s2);
-	printf("%s\n", s1);
-
-	/* char t[] = "Yuan, 2022-08-30"; */
-	/* strcat01(s, t); */
-	/* printf("%s\n", s);	 */
+	int c =	any(s1, s2);
+	printf("%d\n", c);
 }
 
-/* void strcat01(char s[], char t[]) */
-/* { */
-/* 	int i, j; */
-/* 	i = j = 0; */
-/* 	while (s[i] != '\0') { */
-/* 		i++; */
-/* 	} */
-/* 	while ((s[i++] = t[j++]) != '\0'); */
-/* } */
-
-void squeeze(char s1[], char s2[])
+int any(char s1[], char s2[])
 {
 	int i, j;
-	for (int t = 0; s2[t] != '\0'; ++t) {
-		for (i = j = 0; s1[i] != '\0'; ++i) {
-			if (s1[i] != s2[t]) {
-				s1[j++] = s1[i];
+	
+	for (i = 0; s1[i] != '\0'; ++i) {
+		for (j = 0; s2[j] != '\0'; ++j) {
+			if (s2[j] == s1[i]) {
+				return i;
 			}
 		}
-		s1[j] = '\0';
-	}
+	}	
+	return -1;	
 }
