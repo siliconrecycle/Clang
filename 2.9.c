9112b9ec37575ef91ad983d79f46903c641d5fd5
Yuan <2252292157@qq.com>
2022-09-02 16:36:28 +0800

    ex.2-7

--- 2.9.c
+++ 2.9.c
@@ -2,18 +2,47 @@
 
 unsigned getbits(unsigned x, int p, int n);
 unsigned setbits(unsigned x, int p, int n, int y);
+unsigned invert(unsigned x, int p, int n);
 
 int main(void) {
 	int a = 0b01001011;
+	/* int b = 0b01101000; */
 	printf("%#b\n", a);
+	/* printf("%#b\n", b); */
+	/* printf("%#b\n", a ^ b); */
 	/* unsigned int r2 = getbits(a, 3, 3); */
 	/* printf("%#b\n", r2); */
 	/* unsigned int r = getbits(a, 4, 3); */
 	/* printf("%#b\n", r); */
 	/* unsigned int r1 = getbits(a, 5, 3); */
 	/* printf("%#b\n", r1); */
-	int c = setbits(a, 5, 3, 0b00101000);
+	/* int c = setbits(a, 5, 3, 0b00101000); */
+	/* printf("%#b\n", c); */
+	int c = invert(a, 5, 3);
 	printf("%#b\n", c);
+}
+
+/*
+  a = 01001011
+  invert(a, 5, 3) =>
+  01110011
+
+  ~(~0 << 3) << (5 + 1 - 3) =>
+  ~(11111111 << 3) << (3) =>
+  ~(11111000) << 3 =>
+  00000111 << 3 =>
+  00111000
+  b = 00111000 ^
+  a = 01001011 =>
+      01110011
+   */
+
+unsigned invert(unsigned x, int p, int n)
+{
+	int a = x;
+	int b = ~(~0 << n) << (p + 1 - n) ^ a;
+
+	return b;
 }
 
 /*
