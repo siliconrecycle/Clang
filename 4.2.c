cbbc87d74c2a8fee79ba38f9f87a970e753ccf60
Yuan <2252292157@qq.com>
2022-09-29 11:20:21 +0800

    4.2.c

---
+++ 4.2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <ctype.h>
+
+double atof(char s[]);
+
+int main(void) {
+	char f[] = "-1.234";
+	double atof(char s[]);
+	printf("%f\n", atof(f));
+}
+
+double atof(char s[])
+{
+	int i, sign;
+	double val, power;
+	for (i = 0; isspace(s[i]); ++i)
+		;
+	sign = (s[i] == '-') ? -1 : 1;
+	if (s[i] == '-' || s[i] == '+')
+		++i;
+	for (val = 0; isdigit(s[i]); ++i)
+		val = 10 * val + (s[i] - '0');
+	if (s[i] == '.')
+		++i;
+	for (power = 1.0; isdigit(s[i]); ++i) {
+		val = 10 * val + (s[i] - '0');
+		power *= 10;
+	}
+	return sign * val / power;
+}
+		
