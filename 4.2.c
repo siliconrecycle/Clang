58d8b849d218ac64206518b0567df1912413e562
Yuan <2252292157@qq.com>
2022-09-29 15:22:22 +0800

    4.2.c(02)

--- 4.2.c
+++ 4.2.c
@@ -1,31 +1,54 @@
 #include <stdio.h>
 #include <ctype.h>
 
-double atof(char s[]);
+#define MAXLINE 1000
+
+/* double atof(char s[]); */
+
+int atoi(char s[]);
 
 int main(void) {
-	char f[] = "-1.234";
-	double atof(char s[]);
-	printf("%f\n", atof(f));
+	/* char f[] = "-1.234"; */
+	/* double atof(char s[]); */
+	/* printf("%f\n", atof(f)); */
+	
+	char i[] = "123123.1231";
+	printf("%d\n", atoi(i));
+
+	/* double sum, atof(char []); */
+	/* /\* double sum; *\/ */
+	/* char line[MAXLINE]; */
+	/* int getline01(char [], int); */
+	/* sum = 0; */
+	/* while (getline01(line, MAXLINE) > 0) { */
+	/* 	printf("sum: %f\n", sum += atof(line)); */
+	/* } */
+	/* return 0; */
 }
 
-double atof(char s[])
+int atoi(char s[])
 {
-	int i, sign;
-	double val, power;
-	for (i = 0; isspace(s[i]); ++i)
-		;
-	sign = (s[i] == '-') ? -1 : 1;
-	if (s[i] == '-' || s[i] == '+')
-		++i;
-	for (val = 0; isdigit(s[i]); ++i)
-		val = 10 * val + (s[i] - '0');
-	if (s[i] == '.')
-		++i;
-	for (power = 1.0; isdigit(s[i]); ++i) {
-		val = 10 * val + (s[i] - '0');
-		power *= 10;
-	}
-	return sign * val / power;
+	double atof(char []);
+	return (int)atof(s);
 }
+
+/* double atof(char s[]) */
+/* { */
+/* 	int i, sign; */
+/* 	double val, power; */
+/* 	for (i = 0; isspace(s[i]); ++i) */
+/* 		; */
+/* 	sign = (s[i] == '-') ? -1 : 1; */
+/* 	if (s[i] == '-' || s[i] == '+') */
+/* 		++i; */
+/* 	for (val = 0; isdigit(s[i]); ++i) */
+/* 		val = 10 * val + (s[i] - '0'); */
+/* 	if (s[i] == '.') */
+/* 		++i; */
+/* 	for (power = 1.0; isdigit(s[i]); ++i) { */
+/* 		val = 10 * val + (s[i] - '0'); */
+/* 		power *= 10; */
+/* 	} */
+/* 	return sign * val / power; */
+/* } */
 		
