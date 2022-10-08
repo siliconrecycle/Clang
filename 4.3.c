0e4ece7f8edbb4da787f7279601525d2a1193f56
Yuan <2252292157@qq.com>
2022-10-08 17:32:45 +0800

    4.3.c(unfinished)

--- 4.3.c
+++ 4.3.c
@@ -1,119 +1,67 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define MAXNUM 10
+#define MAXVAL 100
 #define NUMBER '0'
 
 int getop(char s[]);
-void push(double f);
+
+int getop(char s[])
+{
+	
+
+void push(double i);
 double pop(void);
 
-int main(void) {
-	int op;
-	char s[MAXNUM];
-	double temp;
-
-	while ((op = getop(s)) != EOF) {
-		printf("op: %c\n", op);
-		switch (op) {
-		case NUMBER:
-			push(atof(s));
-			break;
-		case '+':
-			printf("log in");
-			push(pop() + pop());
-			break;
-		case '*':
-			push(pop() * pop());
-			break;
-		case '-':
-			temp = pop();
-			push(pop() - temp);
-			break;
-		case '/':
-			temp = pop();
-			if (temp != 0.0)
-				push(pop() / temp);
-			else
-				printf("error: zero divisor\n");
-			break;
-		case '\n':
-			printf("result: %f\n", pop());
-			break;
-		default:
-			printf("error: unknown command %s\n", s);
-			break;
-		}
-	}
-	return 0;
-}
-
-#define MAXVAL 100
-
+char val[MAXVAL];
 int sp = 0;
-double val[MAXVAL];
 
-void push(double f)
+void push(double i)
 {
 	if (sp < MAXVAL)
-		val[sp++] = f;
+		val[sp++] = i;
 	else
-		printf("error: stack full, can't push %g\n", f);
+		printf("error: number is too big.\n");
 }
 
 double pop(void)
 {
 	if (sp > 0)
 		return val[--sp];
-	else {
-		printf("error: stack empty\n");
-		return 0.0;
-	}
-}
-
-#include <ctype.h>
-
-int getch(void);
-void ungetch(int);
-
-int getop(char s[])
-{
-	int c, i;
-
-	while ((s[0] = c = getch()) == ' ' || c == '\t')
-		;
-	s[1] = '\0';
-	if (!isdigit(c) && c != '.') {
-		return c;
-	}
-	
-	if (isdigit(c)) {
-		while (isdigit(s[++i] = c = getch()))
-		       ;
-	}
-	if (c == '.')
-		while (isdigit(s[++i] = c = getch()))
-			;
-	s[i] = '\0';
-	if (c != EOF)
-		ungetch(c);
-	return NUMBER;		
-}
-
-#define BUFSIZE 100
-
-char buf[BUFSIZE];
-int bufp = 0;
-
-int getch(void)
-{
-	return (bufp > 0) ? buf[--bufp] : getchar();
-}
-
-void ungetch(int c)
-{
-	if (bufp >= BUFSIZE)
-		printf("ungetch: too many characters\n");
 	else
-		buf[bufp++] = c;
+		printf("error: no number.\n");
+}
+
+int main(void) {
+	int op, op2;
+	char s[MAXVAL];
+
+	while ((op = getop(s)) != EOF) {
+		switch (op) {
+		case NUMBER:
+			push(atof(s));
+			break;
+		case '+':
+			push(pop() + pop());
+			break;
+		case '*':
+			push(pop() * pop());
+			break;
+		case '-':
+			op2 = pop();
+			push(pop() - op2);
+			break;
+		case '/':
+			op2 = pop();
+			if (op == 0.0)
+				printf("error: divider is zero\n");
+			else
+				push(pop() \ op2);
+			break;
+		case '\n':
+			printf("\tresult: %g\n", pop());
+			break;
+		default:
+			printf("error: unknown operator!\n");
+		}
+	}
 }
