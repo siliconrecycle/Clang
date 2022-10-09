36c4fcc71956ba86a73aa61c4fb23ccf6b7e2b24
Yuan <2252292157@qq.com>
2022-10-09 11:29:21 +0800

    4.3.c(02)

--- 4.3.c
+++ 4.3.c
@@ -1,13 +1,44 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 #define MAXVAL 100
 #define NUMBER '0'
+
+void login(double i);
+
+void login(double i)
+{
+	printf("log in: %f\n", i);
+}
 
 int getop(char s[]);
 
 int getop(char s[])
 {
-	
+	int c, i;
+
+       	while ((s[0] = c = getchar()) == ' ' || c == '\t')
+		;
+	s[1] = '\0';
+	if (!isdigit(c))
+		return c;
+	for (i = 1; isdigit(c); ++i) {
+		s[i] = c;
+		c = getchar();
+	}
+	if (c == '.')
+		s[i] = c;
+	for (i = 1; isdigit(c); ++i) {
+		s[i] = c;
+		c = getchar();
+	}	
+
+	s[i] = '\0';
+	/* if (c == '\n') */
+	/* 	return c; */
+	return NUMBER;
+}
 
 void push(double i);
 double pop(void);
@@ -27,8 +58,10 @@ double pop(void)
 {
 	if (sp > 0)
 		return val[--sp];
-	else
-		printf("error: no number.\n");
+	else {
+		printf("error: no number!\n");
+		return 0.0;
+	}
 }
 
 int main(void) {
@@ -38,6 +71,7 @@ int main(void) {
 	while ((op = getop(s)) != EOF) {
 		switch (op) {
 		case NUMBER:
+			login(atof(s));
 			push(atof(s));
 			break;
 		case '+':
@@ -55,7 +89,7 @@ int main(void) {
 			if (op == 0.0)
 				printf("error: divider is zero\n");
 			else
-				push(pop() \ op2);
+				push(pop() / op2);
 			break;
 		case '\n':
 			printf("\tresult: %g\n", pop());
