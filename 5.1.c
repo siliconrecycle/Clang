739ced79c91fe56b91a091177f72f6d0d6fbc6f7
Yuan <2252292157@qq.com>
2022-11-03 16:44:46 +0800

    ex.5-2

--- 5.1.c
+++ 5.1.c
@@ -2,20 +2,21 @@
 
 #define SIZE 10
 
-void printa(int a[], int size)
+void printa(double a[], int size)
 {
 	int i;
 	for(i = 0; i < size; ++i)
-		printf("a[%d]: %d\n", i, a[i]);
+		printf("a[%d]: %g\n", i, a[i]);
 }
 
 int main(void) {
-	int n, array[SIZE], getint(int *np);
+	int n, getfloat(double *np);
+	double array[SIZE];
 
 	for(int i = 0; i < SIZE; i++)
 		array[i] = 1;
 
-	for(n = 0; (n < SIZE) && getint(&array[n]) != EOF; n++)
+	for(n = 0; (n < SIZE) && getfloat(&array[n]) != EOF; n++)
 		;
 	printa(array, SIZE);
 }
