06ac3f5685fc0b58d991ac236984185667379149
Yuan <2252292157@qq.com>
2022-11-10 16:19:49 +0800

    ex.5-3 (char *str) and (char str[])'s differences.
    In this section, i eventually meet the annoying pointer problem.
    When i use pointer in wrong way, it impossible report the right
    result for me, and it even report some unknown result.

--- ex.5-3.c
+++ ex.5-3.c
@@ -1,32 +1,44 @@
 #include <stdio.h>
 
 void login(void);
+
 char *strcat(char *s, char *t);
+void strcat01(char s[], char t[]);
 
 int main(void) {
-	char *h = "hello ";
-	char *w = "Qian";
+	char h[20] = "hello ";
+	char *w = "Qian bcde";
+	/* strcat01(h, w); */
+	/* printf("%s\n", w); */
+
 	printf("%s\n", strcat(h, w));
+
+	/* char *r = w; */
+	/* printf("%s\n", r); */
 }
 
 char *strcat(char *s, char *t)
 {
-	char msg[100];
-	char *p = msg;
-	int i;
-	for(i = 0;*p = *s; p++, s++, i++) {
-		if(*s == '\0')
-			break;
-	}
-	
-	while(*p++ = *t++) {
-		printf("%c\n", *p);
-	}
-	printf("%s\n", msg);
-	return p = &msg[0];
+	char *p = s;
+	while(*s++ != '\0')
+		;
+	s--;
+	while((*s++ = *t++) != '\0')
+		;
+	return p;
 }
 	
 void login(void)
 {
 	printf("log in\n");
+}
+
+void strcat01(char s[], char t[])
+{
+	int i, j;
+	i = j = 0;
+	while(s[i] != '\0')
+		i++;
+	while((s[i++] = t[j++]) != '\0')
+		;
 }
