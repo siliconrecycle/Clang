a37c66872c969a72da2ab2cf819c9a71c668505f
Yuan <2252292157@qq.com>
2023-03-15 08:47:44 -0400

    ex.5-13 init

--- K&R-5-6-3/example/main.c
+++ K&R-5-6-3/example/main.c
@@ -4,12 +4,9 @@
 //void qsort(char *lineptr[], int left, int right);
 //int readlines(char *lineptr[], int maxline);
 //void writelines(char *lineptr[], int num);
-//
+
 //int getline01(char *line, int max);
 //void copyto(char *lineptr[], char *str, int p);
-
-
-
 
 #define MAXLINES 1000
 
